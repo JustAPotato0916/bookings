850190241c2d74f8c8387c84154933b95eba0b8a
JustAPotato0916 <justapotato0916@gmail.com>
2023-03-17 13:06:45 +0800

    Initial commit

---
+++ pkg/render/render.go
@@ -0,0 +1,97 @@
+package render
+
+import (
+	"bytes"
+	"github.com/JustAPotato0916/bookings/pkg/config"
+	"github.com/JustAPotato0916/bookings/pkg/models"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+)
+
+var functions = template.FuncMap{}
+
+var app *config.AppConfig
+
+// NewTemplates sets the config for the template package
+func NewTemplates(a *config.AppConfig) {
+	app = a
+}
+
+func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+	return td
+}
+
+// RenderTemplate renders templates using html/template
+func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
+	var tc map[string]*template.Template
+
+	if app.UseCache {
+		// Get template cache from app config
+		tc = app.TemplateCache
+	} else {
+		tc, _ = CreateTemplateCache()
+	}
+
+	// Get requested template from cache
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatal("Could not get template from template cache")
+	}
+
+	// Create a buffer to hold the template
+	buf := new(bytes.Buffer)
+
+	td = AddDefaultData(td)
+
+	_ = t.Execute(buf, td)
+
+	// Render template
+	_, err := buf.WriteTo(w)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+}
+
+func CreateTemplateCache() (map[string]*template.Template, error) {
+	myCache := map[string]*template.Template{}
+
+	// Get all pages from templates folder named *.page.tmpl
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+	if err != nil {
+		return myCache, err
+	}
+
+	// Loop through pages
+	for _, page := range pages {
+		// Get page
+		name := filepath.Base(page)
+
+		// Parse page template and store in cache
+		ts, err := template.ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		// Get all layouts from templates folder named *.layout.tmpl
+		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+		if err != nil {
+			return myCache, err
+		}
+
+		// If layout exists, add it to the template set
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.tmpl")
+			if err != nil {
+				return myCache, err
+			}
+		}
+
+		// Add template set to cache
+		myCache[name] = ts
+	}
+
+	return myCache, nil
+}
