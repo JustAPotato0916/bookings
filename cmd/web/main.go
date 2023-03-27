eac2760d4c4bd0a3b558467f2acf0e6cd4952fb4
JustAPotato0916 <justapotato0916@gmail.com>
2023-03-27 13:15:48 +0800

    feat: centralize error handling

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,19 +5,22 @@ import (
 	"fmt"
 	"github.com/JustAPotato0916/bookings/internal/config"
 	"github.com/JustAPotato0916/bookings/internal/handlers"
+	"github.com/JustAPotato0916/bookings/internal/helpers"
 	"github.com/JustAPotato0916/bookings/internal/models"
 	"github.com/JustAPotato0916/bookings/internal/render"
 	"github.com/alexedwards/scs/v2"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
 const portNumber = ":8080"
 
 var app config.AppConfig
-
 var session *scs.SessionManager
+var infoLog *log.Logger
+var errorLog *log.Logger
 
 // main is the main application function
 func main() {
@@ -44,6 +47,12 @@ func run() error {
 	// change this to true when in production
 	app.InProduction = false
 
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	app.InfoLog = infoLog
+
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.ErrorLog = errorLog
+
 	session = scs.New()
 	session.Lifetime = 24 * time.Hour
 	session.Cookie.Persist = true
@@ -63,8 +72,8 @@ func run() error {
 
 	repo := handlers.NewRepo(&app)
 	handlers.NewHandlers(repo)
-
 	render.NewTemplates(&app)
+	helpers.NewHelpers(&app)
 
 	return nil
 }
