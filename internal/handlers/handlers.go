d28224ed9d9b7e8ad5fdf495041972603ee5e95e
JustAPotato0916 <justapotato0916@gmail.com>
2023-04-06 21:24:09 +0800

    feat: setting up database function: inserting a reservation

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -12,6 +12,8 @@ import (
 	"github.com/JustAPotato0916/bookings/internal/repository"
 	"github.com/JustAPotato0916/bookings/internal/repository/dbrepo"
 	"net/http"
+	"strconv"
+	"time"
 )
 
 // Repo the repository used by the handlers
@@ -68,11 +70,34 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	sd := r.Form.Get("start_date")
+	ed := r.Form.Get("end_date")
+
+	// 2023-01-01 -- 01/02 03:04:05PM '06 -0700
+
+	layout := "2006-01-02"
+	startDate, err := time.Parse(layout, sd)
+	if err != nil {
+		helpers.ServerError(w, err)
+	}
+	endDate, err := time.Parse(layout, ed)
+	if err != nil {
+		helpers.ServerError(w, err)
+	}
+
+	roomID, err := strconv.Atoi(r.Form.Get("room_id"))
+	if err != nil {
+		helpers.ServerError(w, err)
+	}
+
 	reservation := models.Reservation{
 		FirstName: r.Form.Get("first_name"),
 		LastName:  r.Form.Get("last_name"),
 		Phone:     r.Form.Get("phone"),
 		Email:     r.Form.Get("email"),
+		StartDate: startDate,
+		EndDate:   endDate,
+		RoomID:    roomID,
 	}
 
 	form := forms.New(r.PostForm)
@@ -90,6 +115,11 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 			Data: data,
 		})
 		return
+	}
+
+	err = m.DB.InsertReservation(reservation)
+	if err != nil {
+		helpers.ServerError(w, err)
 	}
 
 	m.App.Session.Put(r.Context(), "reservation", reservation)
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,5 +1,9 @@
 package repository
 
+import "github.com/JustAPotato0916/bookings/internal/models"
+
 type DatabaseRepo interface {
 	AllUsers() bool
+
+	InsertReservation(res models.Reservation) error
 }
