eae9bab852a177a1da266ace8d183a53428fd62f
JustAPotato0916 <justapotato0916@gmail.com>
2023-04-07 11:31:18 +0800

    feat: searching for availability for room

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,10 +1,14 @@
 package repository
 
-import "github.com/JustAPotato0916/bookings/internal/models"
+import (
+	"github.com/JustAPotato0916/bookings/internal/models"
+	"time"
+)
 
 type DatabaseRepo interface {
 	AllUsers() bool
 
 	InsertReservation(res models.Reservation) (int, error)
-	InsertRoomRestriction(r models.RoomRestriction) error
+	InsertRoomRestriction(r models.RoomRestriction)
+	SearchAvailabilityByDatesByRoomID(start, end time.Time, roomID int) (bool, error)
 }
