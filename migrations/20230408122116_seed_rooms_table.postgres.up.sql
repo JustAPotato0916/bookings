0f6c7c2fd8f4df400b8192e8184db467719b8a3e
JustAPotato0916 <justapotato0916@gmail.com>
2023-04-08 20:23:37 +0800

    feat: add migration for seeding rooms

---
+++ migrations/20230408122116_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO "rooms" ("id", "room_name", "created_at", "updated_at") VALUES
+('General''s Quarters', '2023-04-06 00:00:00', '2023-04-06 00:00:00'),
+('Major''s Suite', '2023-04-07 17:05:27', '2023-04-07 17:05:27');
