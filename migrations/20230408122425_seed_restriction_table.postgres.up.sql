e6996e29ddf5aaa04e79142cc262903af568c8f2
JustAPotato0916 <justapotato0916@gmail.com>
2023-04-08 20:26:24 +0800

    feat: add migration for seeding restrictions

---
+++ migrations/20230408122425_seed_restriction_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO "restrictions" ("id", "restriction_name", "created_at", "updated_at") VALUES
+('Reservation', '2023-04-06 21:41:39', '2023-04-06 21:41:42'),
+('Owner Block', '2023-04-07 16:51:30', '2023-04-07 16:51:31');
